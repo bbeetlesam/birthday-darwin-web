4c6d76e512be216ca82ace17e422c42f8c4afcb0
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-06 02:56:26 +0700

    add: Intro page for the opening scene!

---
+++ src/lib/pages/Intro.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import darwinShutImg from '$lib/assets/images/darwin-smileshut.png';
+	import Window from '$lib/components/Window.svelte';
+</script>
+
+<Window
+	title="hello-darwin.exe"
+	width="360px"
+	height="360px"
+	style={{
+		borderRadius: '0.5rem',
+		borderColor: '#2f2f2f',
+		borderWidth: '2px',
+		statusbarColor: '#f2d3dd',
+		statusbarSeparatorColor: '#2f2f2f',
+		statusbarSeparatorWidth: '2px',
+		contentPadding: '0rem'
+	}}
+>
+	<div class="flex h-full w-full items-center justify-center">
+		<img src={darwinShutImg} alt="Darwin" class="h-full w-full object-cover" />
+	</div>
+</Window>
