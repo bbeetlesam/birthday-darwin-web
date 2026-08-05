4a9e9b4a7f723188baa36de43728d71591c89479
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-05 15:19:20 +0700

    init.. do not tell anyone
    that this is for her
    
    love her

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kamil's Secret Birthday Party
+
+Unwanted guests are prohibited from attending the party. Please RSVP.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import myImage from '$lib/assets/images/darwin1.png';
+</script>
+
+<main class="flex min-h-screen flex-col items-center justify-center gap-4 p-8 text-center">
+	<h1 class="text-3xl font-bold">Welcome to Kamil's Secret Birthday Party</h1>
+
+	<p class="text-lg">Visit her party room to see the documentation</p>
+
+	<img src={myImage} alt="Description" class="h-auto w-72" />
+</main>
