85678dc724e66c1094972dab0b02a854ee66df85
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-06 04:24:01 +0700

    feat: set scene manager in +page, and add Darwin for the second scene
    currently Darwin is empty (no pun intended)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,20 +2,36 @@
 	import MovingBg from '$lib/components/MovingPatternBackground.svelte';
 	import flowerBgImg from '$lib/assets/images/flower-rg.png';
 	import Intro from '$lib/pages/Intro.svelte';
+	import WhosDarwin from '$lib/pages/Darwin.svelte';
+
+	const pages = ['intro', 'darwin'] as const;
+	type Scene = (typeof pages)[number];
+
+	let scene = $state<Scene>(pages[0]);
+
+	function goToScene(nextScene: Scene) {
+		scene = nextScene;
+	}
 </script>
 
+<!-- the lovely flowery background -->
 <MovingBg
 	image={flowerBgImg}
 	backgroundColor="#FDFBF7"
 	size="48px"
 	opacity={0.12}
 	angle="-20deg"
-	speed="18s"
+	speed="20s"
 	distance="280px"
 />
 
-<main class="flex min-h-screen flex-col items-center justify-center gap-4 p-8 text-center">
-	<h1 class="text-3xl font-bold">Welcome to Kamil's Secret Birthday Party</h1>
-	<p class="text-lg">Visit her party room to see the documentation</p>
-</main>
-<Intro />
+<!-- REMOVE IN PROD -->
+<!-- only for debugging -->
+<p>${scene}</p>
+
+<!-- scene manager goes hard here -->
+{#if scene === 'intro'}
+	<Intro onComplete={goToScene} />
+{:else if scene === 'darwin'}
+	<WhosDarwin />
+{/if}
