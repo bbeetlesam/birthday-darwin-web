3275e83b1228f025518f867a19ffcb8736dd5df7
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-06 16:38:24 +0700

    added Darwin.svelte as the second scene
    will be used later in scene transition

---
+++ src/lib/pages/Darwin.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Window from '$lib/components/Window.svelte';
+</script>
+
+<Window
+  title="Who is Darwin?"
+  initialX={window.innerWidth / 2 - 200}
+  initialY={window.innerHeight / 2 - 150}
+  width="400px"
+  height="300px"
+  style={{
+    borderRadius: '0.5rem',
+    borderColor: '#2f2f2f',
+    borderWidth: '2px',
+    statusbarColor: '#f2d3dd',
+    statusbarSeparatorColor: '#2f2f2f',
+    statusbarSeparatorWidth: '2px',
+    contentPadding: '1rem'
+  }}
+>
+  <p>just a placeholder window</p>
+</Window>
