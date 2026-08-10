668793870d600c76bcf3177b4729a02883051737
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-10 20:40:49 +0700

    added more gallery entries! (10)

--- src/lib/pages/darwin/gallery-entries.ts
+++ src/lib/pages/darwin/gallery-entries.ts
@@ -11,6 +11,18 @@ import bizarreDarwinImg from '$lib/assets/images/darwin-bizarre.png';
 import laughingDarwinImg from '$lib/assets/images/darwin-laugh.png';
 import pitifulDarwinImg from '$lib/assets/images/darwin-pitiful.png';
 import joyDarwinImg from '$lib/assets/images/darwin-joy.png';
+import listeningDarwinImg from '$lib/assets/images/darwin-listening.png';
+import dollDarwinImg from '$lib/assets/images/darwin-doll.png';
+import cryingDarwinImg from '$lib/assets/images/darwin-crying.png';
+import writingDarwinImg from '$lib/assets/images/darwin-writing.png';
+import screamsDarwinImg from '$lib/assets/images/darwin-screams.png';
+import goofyDarwinImg from '$lib/assets/images/darwin-goofy.png';
+import pufferfishDarwinImg from '$lib/assets/images/darwin-pufferfish.png';
+import kungfuDarwinImg from '$lib/assets/images/darwin-kungfu.png';
+import smartDarwinImg from '$lib/assets/images/darwin-smart.png';
+import angryDarwinImg from '$lib/assets/images/darwin-angry.png';
+import bizarre2DarwinImg from '$lib/assets/images/darwin-bizarre2.png';
+import nyenyeDarwinImg from '$lib/assets/images/darwin-nyenye.png';
 
 export const galleryEntries: GalleryItem[] = [
 	{
@@ -108,5 +120,104 @@ export const galleryEntries: GalleryItem[] = [
 		shortDesc: 'I hope you understand...',
 		longDesc: `Though I spend my days in conversation.. Please remember me.. Take a good look around you.. Take a good look, you're bound to see.. That you and me...
       My dear.. You have always been my inspiration.. Please, be good to me..`
+	},
+	{
+		id: 'listening-darwin',
+		title: 'Honey Pie.',
+		image: listeningDarwinImg,
+		shortDesc: 'Acting like a nonchalant guy.',
+		longDesc: `This song, I mean Honey Pie by The Beatles, is good. Yeah, good. That's it. Actually deep
+      inside of my heart I think this song melts my heart quite a part..`
+	},
+	{
+		id: 'doll-darwin',
+		title: 'Jolly Dolly Piñata!',
+		image: dollDarwinImg,
+		shortDesc: 'Are there Rupiahs inside?',
+		longDesc: `"I am Jolly Dolly Donky!", said Jolly Dolly Donky to Funky Goofy Darwin, as they both stare
+      each other and probably thinking why did they have to pose like such at the first place.`
+	},
+	{
+		id: 'writing-darwin',
+		title: 'Calculating almost anything',
+		image: writingDarwinImg,
+		shortDesc:
+			'Should I spend this in that or that in this, so therefore I got this, this, and this?',
+		longDesc: `A book can be scary, if you read it with the wrong mindset. Try to start with some simple words, and
+      perhaps some drawings too, I guess? Nothing is scary, anyway. Just overwhelming.`
+	},
+	{
+		id: 'crying-darwin',
+		title: "I'm fine..",
+		image: cryingDarwinImg,
+		shortDesc: 'I must believe that a strong person is I am.',
+		longDesc: `Sunrise doesn't last all morning. A cloudburst doesn't last all day. The darkness only stays at
+      nighttime, in the morning it will fade away. It's not always gonna be this grey. This too shall pass.. soon..`
+	},
+	{
+		id: 'screams-darwin',
+		title: "I'VE HAD ENOUGH",
+		image: screamsDarwinImg,
+		shortDesc: 'THIS IS MY LAST STRAW.',
+		longDesc: `CAN'T DREAMS ALL DAY AND DO NOTHING.`
+	},
+	{
+		id: 'goofy-darwin',
+		title: 'bibadubibubadibako',
+		image: goofyDarwinImg,
+		shortDesc: "I'm a fool I don't know what I'm doing lalala",
+		longDesc: `This is me when I have no idea what I'm doing, because standing in front of me is one of the pinnacle of the nature's
+      creation, and she is making me feel like she is doing something I don't understand.`
+	},
+	{
+		id: 'pufferfish-darwin',
+		title: 'Puffer-fish',
+		image: pufferfishDarwinImg,
+		shortDesc: 'Not a goldfish anyhow.',
+		longDesc: `I feel bloated 'cause I'm a pufferfish. I feel fat 'cause I'm a pufferfish. I feel full 'cause I'm a pufferfish.
+      Living as a goldfish must be a fun. But even better to live alongside your love.`
+	},
+	{
+		id: 'angry-darwin',
+		title: 'Never again.',
+		image: angryDarwinImg,
+		shortDesc: 'Not quite my tempo.',
+		longDesc: `Some kind of happiness is measured out in miles. What makes me think I'm something special when I smile?
+      Some kind of innocence is measured out in years. I don't know what it's like to listen to my fears.
+      Some kind of solitude is measured out in me. I think I know me, but I haven't got a clue.
+      Frightened of the dark, I don't know what to do anymore. Cry!`
+	},
+	{
+		id: 'kungfu-darwin',
+		title: 'Enter the Dragon!',
+		image: kungfuDarwinImg,
+		shortDesc: 'Persistence over spontaneous',
+		longDesc: `I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times!
+      Let that love in.`
+	},
+	{
+		id: 'smart-darwin',
+		title: 'Charles Darwin Watterson',
+		image: smartDarwinImg,
+		shortDesc: 'The father of evolutionary goldfish.',
+		longDesc: `I think the state of human being can be described as a combination of love, passion, and resilience, with sprinkles of hate and unfortunates.
+      We can't resist the thought of evil because that's part of us. However, we can resist the urge to actualise it. Put simply, we can't change the world,
+      but we can change ourselves. One little post script, we can't force love from anyone, but we certainly sure can spread love to anyone. I think I would
+      like to make a relationship with you.`
+	},
+	{
+		id: 'bizarre2-darwin',
+		title: 'Flabbergasted',
+		image: bizarre2DarwinImg,
+		shortDesc: 'I feel stimulated',
+		longDesc: `A smell of a victorious moment can't be this pleasurable! What moment? Everything depends on our own interpretation!`
+	},
+	{
+		id: 'nyenye-darwin',
+		title: 'Nyenyenye',
+		image: nyenyeDarwinImg,
+		shortDesc: 'Yo ndak tau kok tanya saya.',
+		longDesc: `Have already told you about that, and you won't listen. Now that it's finally happened, I fell in love with you,
+      now you can do nothing about it.`
 	}
 ];
