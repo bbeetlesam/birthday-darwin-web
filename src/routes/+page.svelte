3fe146c095c3f62d0ffa319796bae5a77b5fa001
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-11 00:36:28 +0700

    remove debugging scene text in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
 
 <!-- REMOVE IN PROD -->
 <!-- only for debugging -->
-<p class="fixed top-0 left-0 z-9999">${scene}</p>
+<!-- <p class="fixed top-0 left-0 z-9999">${scene}</p> -->
 
 <!-- scene manager goes hard here -->
 <SceneManager bind:scene />
