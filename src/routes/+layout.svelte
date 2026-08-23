72f4a3c8a7d71f5384711acb31746022effe83c4
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-23 17:17:48 +0700

    refactor: put MovingBg to root +layout instead of root +page (so every scene/route can have it all)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import './layout.css';
 	import Boot from '$lib/components/Boot.svelte';
+	import MovingBg from '$lib/components/MovingPatternBackground.svelte';
 	import favicon from '$lib/assets/favicon.svg';
+	import flowerBgImg from '$lib/assets/images/flower-rg.png';
 
 	let { children } = $props();
 </script>
@@ -10,6 +12,18 @@
 	<title>Rahasia Y.</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
 <Boot>
+  <!-- Delicate moving flower background -->
+  <MovingBg
+	image={flowerBgImg}
+	backgroundColor="#FDFBF7"
+	size="48px"
+	opacity={0.12}
+	angle="-20deg"
+	speed="20s"
+	distance="280px"
+  />
+
 	{@render children()}
 </Boot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
-	import MovingBg from '$lib/components/MovingPatternBackground.svelte';
 	import SceneManager from '$lib/components/SceneManager.svelte';
-	import flowerBgImg from '$lib/assets/images/flower-rg.png';
 	import type { Scene } from '$lib/components/SceneManager.svelte';
 
 	let scene = $state<Scene>('intro');
 </script>
-
-<!-- the lovely flowery background -->
-<MovingBg
-	image={flowerBgImg}
-	backgroundColor="#FDFBF7"
-	size="48px"
-	opacity={0.12}
-	angle="-20deg"
-	speed="20s"
-	distance="280px"
-/>
 
 <!-- REMOVE IN PROD -->
 <!-- only for debugging -->
