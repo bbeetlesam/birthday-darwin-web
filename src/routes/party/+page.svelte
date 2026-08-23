46aa481afc948a05b3359fca9b7c3753666785db
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-23 17:22:10 +0700

    feat: added /party route with Party scene for direct enter to Party
    simplify things, for my beautiful girl!

---
+++ src/routes/party/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import SceneManager from '$lib/components/SceneManager.svelte';
+	import type { Scene } from '$lib/components/SceneManager.svelte';
+
+	let scene: Scene = 'party';
+</script>
+
+<!-- Party scene goes hard here! -->
+<SceneManager scene={scene} />
